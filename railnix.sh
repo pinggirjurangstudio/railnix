c0950eca31a19eca844d257a155104533fbc1764
Billy Zaelani Malik <bzm@pinggirjurang.studio>
2026-04-13 02:44:53 +0700

    Add deploy message

--- railnix.sh
+++ railnix.sh
@@ -26,19 +26,24 @@ provision() {
 }
 
 deploy() {
-  local project_name project_id environment plan services
+  local project_name project_id environment plan services message
   project_name=$(tofu output -raw project_name)
   project_id=$(tofu output -raw project_id)
   environment=$1
   plan=$(nix eval .#lib.generateDeploymentPlan --apply "f: f \"$environment\"" --json)
   services=$(echo "$plan" | jq -r 'keys[]')
+  message=${2:-""}
+  local message_flag=()
+  if [[ -n "$message" ]]; then
+    message_flag=(-m "$message")
+  fi
   echo "[railnix] deploy '$project_name($environment)'..."
   for service in $services; do
     config=$(echo "$plan" | jq -c ".\"$service\".config")
     echo "[railnix] generate railway.json for '$service'..."
     echo "$config" | jq . > railway.json
     echo "[railnix] deploy '$service'..."
-    railway up --ci --project "$project_id" --environment "$environment" --service "$service"
+    railway up --ci --project "$project_id" --environment "$environment" --service "$service" "${message_flag[@]}"
   done
   echo "[railnix] done"
 }
@@ -51,7 +56,7 @@ cleanup() {
 main() {
   local cmd
   if [ $# -eq 0 ]; then
-    echo "Usage: railnix {init|plan|up <environment>}"
+    echo "Usage: railnix {init|plan|up <environment> [-m <message>]}"
     exit 0
   fi            
   cmd=''${1:-}
@@ -79,7 +84,7 @@ main() {
       cleanup
       ;;
     *)
-      echo "Usage: railnix {init|plan|up <environment>}"
+      echo "Usage: railnix {init|plan|up <environment> [-m <message>]}"
       exit 1
       ;;
   esac
